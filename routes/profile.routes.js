1121042d877ffe707ac42ab38513f10f258b1895
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-10-20 10:10:04 +0200

    authorization done

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/User.model")
 
-const { isLoggedIn } = require("../middlewares/auth.middlewares.js")
+const { isLoggedIn, isAdmin } = require("../middlewares/auth.middlewares.js")
 
 // aqui van nuestras rutas de prefil (verlo, actualizarlo, ver usuarios, etc)
 // GET "/profile" => el usuario puede ver su perfil
@@ -26,6 +26,13 @@ router.get("/", isLoggedIn, (req, res, next) => {
   .catch((err) => {
     next(err)
   })
+
+})
+
+// GET "/profile/admin-dashboard" => ruta para que el admin tenga acceso a editar cualquier perfil
+router.get("/admin-dashboard", isLoggedIn, isAdmin, (req, res, next) => {
+
+  res.render("profile/admin-dashboard.hbs")
 
 })
 
